87beabb985768bbf9b6cddc01e13123624a96e47
nchertolin <nchertolin@gmail.com>
2023-05-27 16:01:07 +0500

    4.16. Разделяй и властвуй

---
+++ src/view/empty-list-view.js
@@ -0,0 +1,14 @@
+import AbstractView from '../framework/view/abstract-view.js';
+
+const createEmptyListTemplate = () => (
+  '<p class="trip-events__msg">Click New Event to create your first point</p>'
+);
+
+export default class EmptyListView extends AbstractView {
+
+  get template() {
+    return createEmptyListTemplate();
+  }
+
+}
+
